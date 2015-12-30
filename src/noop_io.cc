f4b29765b536fb57645adcf99c99d597910b6299
Magica <i@0u0.moe>
2015-12-30 09:16:01 +0800

    Fix api

--- src/noop_io.cc
+++ src/noop_io.cc
@@ -11,22 +11,23 @@ namespace noop {
 
 wstring_convert<codecvt_utf8_utf16<char16_t>, char16_t> cvt;
 
+namespace Encoding {
+  u16string UTF8ToUTF16(string u8_str) {
+    return cvt.from_bytes(u8_str);
+  }
+  string UTF16ToUTF8(u16string u16_str) {
+    return cvt.to_bytes(u16_str);
+  }
+} // namespace Encoding
+
 ostream& operator <<(ostream& out, const u16string u16_str) {
-  string u8_str = cvt.to_bytes(u16_str);
-  out << u8_str;
-  return out;
+  string u8_str = Encoding::UTF16ToUTF8(u16_str);
+  return out << u8_str;
 }
 
 ostream& operator <<(ostream& out, const char16_t u16_chr) {
   u16string u16_str = u"";
-  u16_str += u16_chr;
-  return (out << u16_str);
+  return out << (u16_str + u16_chr);
 }
 
-namespace Encoding {
-u16string UTF8ToUTF16(string u8_str) {
-  return cvt.from_bytes(u8_str);
-}
-
-} // namespace Encoding
 } // namespace noop
